9cc597c1e76b4da128b0960c64ded10a3dd14429
Victoria <vikcalm01@gmail.com>
2021-11-23 12:00:56 +0300

    NOT WORKING

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+void sum_of_num_div_bigger_INEFF()
+{
+    vector<int> qttys_by_mod_40(40, 0);
 
+    for (int i(0); i)
+    int n(0);
+    cin >> n;
+    while (n != -9999)
+    {
+        cin >> n;
+        qttys_by_mod_40[n%40] ++;
+
+    }
+    for ()
+
+}
 
 int main()
 {
